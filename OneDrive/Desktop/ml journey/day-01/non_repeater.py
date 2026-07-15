46cd34332fc339d8d9082d15410e6bb9125e5b7a
vishujeet1111 <vishujeet994@gmail.com>
2026-07-15 10:06:35 +0530

    Clean repository structure

--- OneDrive/Desktop/ml journey/day-01/non_repeater.py
+++
@@ -1,5 +0,0 @@
-str = "vishujeet sharma"
-
-for char in str : 
-    if str.count(char) == 1:
-        print(char)
--- OneDrive/Desktop/ml journey/day-01/reverse.py
+++
@@ -1,7 +0,0 @@
-# string reverse using loops 
-
-str = "python"
-reverse_str = "" 
-for char in str :
-    reverse_str = char + reverse_str
-print(reverse_str)
